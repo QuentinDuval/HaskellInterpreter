f8158e3ce1faa76f4b8b15c690b6343a2b0c4f38
Duval Quentin <duvalquentin@macbook-pro-de-duval.home>
2016-10-30 19:15:38 +0100

    Start adding the basics in terms of parser factory and combinators

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@ module Parser where
 
 import Control.Arrow(first, second)
 import Control.Applicative
+import Control.Monad
 
 
 --
@@ -18,7 +19,6 @@ instance IStream String where
   isEmpty = null
   front = head
   popFront = tail
-
 
 
 --
@@ -79,5 +79,35 @@ instance Monad (Parser stream) where
     Parser $ \s1 -> do
       (a, s2) <- pa s1
       runParser (f a) s2
+
+
+--
+-- Parser factories and combinators
+--
+
+parseChar :: (IStream s) => Parser s Char
+parseChar =
+  Parser $ \s ->
+    if isEmpty s
+      then Error "Empty stream!"
+      else Success (front s, popFront s)
+
+raiseError :: String -> Parser s a
+raiseError msg = Parser $ \_ -> Error msg
+
+skipSpace :: (IStream s) => Parser s ()
+skipSpace = do
+  c <- parseChar
+  when (c /= ' ') $
+    raiseError "No space found"
+
+
+-- TEST DRIVER
+
+testParser :: IO ()
+testParser = do
+  let p = (,) <$> parseChar <*> parseChar
+  print $ runParser p "Hello"
+
 
 --
